8c4d7ea0ecc4dc7053c79e164f87314784f9d7e6
Leandro Klyte <klyte45@hotmail.com>
2022-11-18 17:14:57 -0300

    v3.0.1r1 - Fixed UI scale in non-1080p based resolutions

--- ElectricRoadsKw/ModInstance.cs
+++ ElectricRoadsKw/ModInstance.cs
@@ -7,7 +7,7 @@ using System.Globalization;
 using System.Reflection;
 using UnityEngine;
 
-[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyVersion("3.0.1.1")]
 namespace ElectricRoads
 {
     public class ModInstance : BasicIUserMod<ModInstance, MainController>
